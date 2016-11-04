9a59c83e8800d0b94809fae485f72b618f1795ae
winwinastro <winwinnaing447@gmail.com>
2016-11-04 14:12:37 +0630

    .

--- app/controllers/database.js
+++ app/controllers/database.js
@@ -20,147 +20,148 @@ var connection = mysql.createConnection({
 // Connect database
 connection.connect(function (err) {
     if (err) {
-        console.error('Error connecting: ' + err.stack);
+        console.error('*** Error connecting: ' + err.stack);
     } else {
         console.log('Connected as ID: ' + connection.threadId);
-        connection.query('select * from Movie'
-            , function (err, result) {
+        connection.query('select * from Movie', function (err, result) {
 
-                // console.log(req.body);
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log(result);
-                }
-                
-            });
+            // console.log(req.body);
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(result);
+            }
 
-        // insert user
-        exports.add_user = function (req, res) {
-            var query = connection.query("Insert into user set ?", req.body, function (req, res) {
-                if (err) {
-                    return res.status(400).send(err);
-                }
-            });
+        });
+    }
+});
+
+// insert user
+exports.add_user = function (req, res) {
+    var query = connection.query("Insert into user set ?", req.body, function (req, res) {
+        if (err) {
+            return res.status(400).send(err);
         }
+    });
+}
 
-        // select popular
-        exports.get_popular = function (req, res) {
+// select popular
+exports.get_popular = function (req, res) {
 
-            var query = connection.query('select distinct m.* from Movie m join Movie_Genre g on m.movieId = g.movieId join Register r on r.genreId = g.genreId where r.userId = 1 order by vote_average DESC,release_date DESC limit 0,10'
-                , function (err, result) {
+    var query = connection.query('select distinct m.* from Movie m join Movie_Genre g on m.movieId = g.movieId join Register r on r.genreId = g.genreId where r.userId = 1 order by vote_average DESC,release_date DESC limit 0,10'
+        , function (err, result) {
 
-                    // console.log(req.body);
-                    if (err) {
-                        return res.status(400).send(err);
-                    } else {
-                        if (result.length > 0) {
-                            return res.status(200).send(result);
-                        } else {
-                            return res.status(201).send('');
-                        }
-                    }
-                });
-        }
-
-        // select all popular
-        exports.get_allpopular = function (req, res) {
-
-            var query = connection.query('select distinct m.* from Movie m join Movie_Genre g on m.movieId = g.movieId join Register r on r.genreId = g.genreId where r.userId = 1 order by vote_average DESC,release_date DESC limit 0,100'
-                , function (err, result) {
-
-                    // console.log(req.body);
-                    if (err) {
-                        return res.status(400).send(err);
-                    } else {
-                        if (result.length > 0) {
-                            return res.status(200).send(result);
-                        } else {
-                            return res.status(201).send('');
-                        }
-                    }
-                });
-        }
-
-        // select movie detail
-        exports.get_detail = function (req, res) {
-
-            var query = connection.query('select * from Movie where movieId = ?', req.params.movieId, function (err, result) {
-                if (err) {
-                    return res.status(400).send(err);
+            // console.log(req.body);
+            if (err) {
+                return res.status(400).send(err);
+            } else {
+                if (result.length > 0) {
+                    return res.status(200).send(result);
                 } else {
-                    if (result.length > 0) {
-                        return res.status(200).send(result[0]);
-                    } else {
-                        return res.status(201).send('');
-                    }
+                    return res.status(201).send('');
                 }
+            }
+        });
+}
 
-            });
-        }
+// select all popular
+exports.get_allpopular = function (req, res) {
 
-        //select genre
-        exports.get_genre = function (req, res) {
+    var query = connection.query('select distinct m.* from Movie m join Movie_Genre g on m.movieId = g.movieId join Register r on r.genreId = g.genreId where r.userId = 1 order by vote_average DESC,release_date DESC limit 0,100'
+        , function (err, result) {
 
-            var query = connection.query('select g.genreName from Genre g join Movie_Genre mg on g.genreId = mg.genreId where mg.movieId = ?', req.params.movieId, function (err, result) {
-                if (err) {
-                    return res.status(400).send(err);
+            // console.log(req.body);
+            if (err) {
+                return res.status(400).send(err);
+            } else {
+                if (result.length > 0) {
+                    return res.status(200).send(result);
                 } else {
-                    if (result.length > 0) {
-                        return res.status(200).send(result);
-                    } else {
-                        return res.status(201).send('');
-                    }
+                    return res.status(201).send('');
                 }
+            }
+        });
+}
 
-            });
+// select movie detail
+exports.get_detail = function (req, res) {
+
+    var query = connection.query('select * from Movie where movieId = ?', req.params.movieId, function (err, result) {
+        if (err) {
+            return res.status(400).send(err);
+        } else {
+            if (result.length > 0) {
+                return res.status(200).send(result[0]);
+            } else {
+                return res.status(201).send('');
+            }
         }
 
-        // Insert Recommendation
-        exports.add_recommendations = function (req, res) {
+    });
+}
 
-            var query = connection.query('insert ignore into Recommendation set ?', req.body, function (err, result) {
-                if (err) {
-                    return res.status(400).send(err);
-                } else {
-                    if (result.length > 0) {
-                        return res.status(200).send(result);
-                    } else {
-                        return res.status(201).send('');
-                    }
-                }
-            });
+//select genre
+exports.get_genre = function (req, res) {
+
+    var query = connection.query('select g.genreName from Genre g join Movie_Genre mg on g.genreId = mg.genreId where mg.movieId = ?', req.params.movieId, function (err, result) {
+        if (err) {
+            return res.status(400).send(err);
+        } else {
+            if (result.length > 0) {
+                return res.status(200).send(result);
+            } else {
+                return res.status(201).send('');
+            }
         }
 
-        // Insert Like Movies
-        exports.add_likeMovies = function (req, res) {
+    });
+}
 
-            var query = connection.query('insert ignore into Like_Movies set ?', req.body, function (err, result) {
-                if (err) {
-                    return res.status(400).send(err);
-                } else {
-                    if (result.length > 0) {
-                        return res.status(200).send(result);
-                    } else {
-                        return res.status(201).send('');
-                    }
-                }
-            });
+// Insert Recommendation
+exports.add_recommendations = function (req, res) {
+
+    var query = connection.query('insert ignore into Recommendation set ?', req.body, function (err, result) {
+        if (err) {
+            return res.status(400).send(err);
+        } else {
+            if (result.length > 0) {
+                return res.status(200).send(result);
+            } else {
+                return res.status(201).send('');
+            }
         }
+    });
+}
 
-        // Insert Like Genre
-        exports.add_likeGenre = function (req, res) {
+// Insert Like Movies
+exports.add_likeMovies = function (req, res) {
 
-            var query = connection.query('insert ignore into Like_Genre set ?', req.body, function (err, result) {
-                if (err) {
-                    return res.status(400).send(err);
-                } else {
-                    if (result.length > 0) {
-                        return res.status(200).send(result);
-                    } else {
-                        return res.status(201).send('');
-                    }
-                }
-            });
+    var query = connection.query('insert ignore into Like_Movies set ?', req.body, function (err, result) {
+        if (err) {
+            return res.status(400).send(err);
+        } else {
+            if (result.length > 0) {
+                return res.status(200).send(result);
+            } else {
+                return res.status(201).send('');
+            }
         }
+    });
+}
+
+// Insert Like Genre
+exports.add_likeGenre = function (req, res) {
+
+    var query = connection.query('insert ignore into Like_Genre set ?', req.body, function (err, result) {
+        if (err) {
+            return res.status(400).send(err);
+        } else {
+            if (result.length > 0) {
+                return res.status(200).send(result);
+            } else {
+                return res.status(201).send('');
+            }
+        }
+    });
+}
